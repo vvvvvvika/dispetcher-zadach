afa4b049a945cff1753efcaa24237ee5e8c17e6e
vvvvvvika <gramovvich@gmail.com>
2023-02-05 22:31:12 +0300

    диспетчер задач

---
+++ MyProcesses.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class MyProcesses
+    {
+        private static bool InTheMainMenu = true;
+        private static Process Proc = new Process();
+        public static void DrewProcesses(List<Process> list)
+        {
+            foreach (var element in list)
+            {
+                Console.WriteLine("  Название: " + element.ProcessName + " Приоритет: " + element.BasePriority + " Память: " + element.PagedMemorySize64 / 1048576 + "мб");
+            }
+        }
+        public static void DrewProcesses(Process proc)
+        {
+            try
+            {
+                Console.WriteLine("  Название процесса: " + proc.ProcessName);
+                Console.WriteLine("  Использование диска: " + proc.PagedMemorySize64);
+                Console.WriteLine("  Приоритет: " + proc.BasePriority);
+                Console.WriteLine("  Класс приоритета: " + proc.PriorityClass);
+                Console.WriteLine("  Время использование процесса: " + proc.StartTime);
+                Console.WriteLine("Завершить процесс - D");
+                Console.WriteLine("Завершить все процессы с этим именем - Del");
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("  Ошибка: отказанно в доступе");
+            }
+            finally
+            {
+                Console.WriteLine("Выйти в меню процессов - Escape");
+            }
+            
+        }
+        public static int[] ChangeProcess(Process proc)
+        {
+            if (InTheMainMenu)
+            {
+                Console.Clear();
+                Proc = proc;
+                InTheMainMenu = false;
+                DrewProcesses(proc);
+                return new int[2] { 1, 0 };
+            }
+            return null;
+        }
+        public static int[] BackToMenu(List<Process> list)
+        {
+            if (!InTheMainMenu)
+            {
+                Console.Clear();
+                InTheMainMenu = true;
+                DrewProcesses(list);
+                return new int[2] { list.Count, 0 };
+            }
+            return null;
+        }
+        public static int[] StopThisProcess()
+        {
+            if (!InTheMainMenu)
+            {
+                try
+                {
+                    Proc.Kill();
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("  Ошибка: Нету прав для закрытия данного процесса");
+                    DrewProcesses(Proc);
+                    return null;
+                }
+            }
+            return BackToMenu(Process.GetProcesses().ToList());
+        }
+        public static int[] StopAllProcesses(List<Process> list)
+        {
+            if (!InTheMainMenu)
+            {
+                try
+                {
+                    Proc.Kill();
+                    foreach(var element in list)
+                    {
+                        if(element.ProcessName == Proc.ProcessName)
+                        {
+                            element.Kill();
+                        }
+                    }
+                    Console.Clear();
+                }
+                catch
+                {
+                    Console.Clear();
+                    Console.WriteLine("  Ошибка: Нету прав для закрытия данного процесса");
+                    DrewProcesses(Proc);
+                    return null;
+                }
+            }
+            return BackToMenu(Process.GetProcesses().ToList());
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using System.Dynamic;
+
+namespace ConsoleApp1
+{
+    internal enum HotKey
+    {
+        УдалитьОдинПроцесс = ConsoleKey.D,
+        УдалитьОдинаковыеПроцессы = ConsoleKey.Delete,
+        Выбрать = ConsoleKey.Enter,
+        Вверх = ConsoleKey.UpArrow,
+        Вниз = ConsoleKey.DownArrow,
+        Назад = ConsoleKey.Escape
+    }
+    internal class Program
+    {
+        private static int y = 0;
+        private static int lastY;
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            MyProcesses.DrewProcesses(Process.GetProcesses().ToList());
+            int MaxPositionArrow = Process.GetProcesses().Length;
+            int MinPositionArrow = 0;
+            while (true)
+            {
+                ConsoleKey key = ArrowMenu(MinPositionArrow, MaxPositionArrow-1);
+                if (key == (ConsoleKey)HotKey.Выбрать)
+                {
+                    lastY = y;
+                    int[] arr = MyProcesses.ChangeProcess(Process.GetProcesses()[y]);
+                    if (arr != null)
+                    {
+                        MaxPositionArrow = arr[0];
+                        MinPositionArrow = arr[1];
+                    }
+                    y = 0;
+                }
+                else if (key == (ConsoleKey)HotKey.УдалитьОдинПроцесс)
+                {
+                    int[] arr = MyProcesses.StopThisProcess();
+                    if(arr != null)
+                    {
+                        MaxPositionArrow = arr[0];
+                        MinPositionArrow = arr[1];
+                        y = lastY;
+                    }
+                }
+                else if (key == (ConsoleKey)HotKey.УдалитьОдинаковыеПроцессы)
+                {
+                    int[] arr = MyProcesses.StopAllProcesses(Process.GetProcesses().ToList());
+                    if (arr != null)
+                    {
+                        MaxPositionArrow = arr[0];
+                        MinPositionArrow = arr[1];
+                        y = Process.GetProcesses().Length;
+                    }
+                }
+                else if (key == (ConsoleKey)HotKey.Назад)
+                {
+                    int[] arr = MyProcesses.BackToMenu(Process.GetProcesses().ToList());
+                    if (arr != null)
+                    {
+                        MaxPositionArrow = arr[0];
+                        MinPositionArrow = arr[1];
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    y = lastY;
+                }
+            }
+        }
+        private static ConsoleKey ArrowMenu(int MinPositionArrow ,int MaxPositionArrow)
+        {
+            int LastPosY = 0;
+            Console.SetCursorPosition(0, y);
+            Console.Write("->");
+            ConsoleKey key = Console.ReadKey().Key;
+            if (key == (ConsoleKey)HotKey.Вверх)
+            {
+                LastPosY = y--;
+                if (y < MinPositionArrow)
+                {
+                    LastPosY = MinPositionArrow;
+                    y = MaxPositionArrow;
+                }
+                ClearLatPositionArrow(LastPosY);
+            }
+            else if (key == (ConsoleKey)HotKey.Вниз)
+            {
+                LastPosY = y++;
+                if (y >= MaxPositionArrow)
+                {
+                    y = MinPositionArrow;
+                }
+                ClearLatPositionArrow(LastPosY);
+            }
+
+            return key;
+        }
+        private static void ClearLatPositionArrow(int LastPosY)
+        {
+            Console.SetCursorPosition(0, LastPosY);
+            Console.Write("  ");
+        }
+    }
+}
